012314f1ef439093016d7b957373d5f09d05d0b7
Alyona <alyona.createbox@gmail.com>
2024-10-09 18:43:13 +0300

    Finished ListingDetailView

--- AirbnbTutorial/Core/Explore/ExploreView.swift
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -27,7 +27,8 @@ struct ExploreView: View {
                     .padding()
                 }
                 .navigationDestination(for: Int.self) { listing in
-                    Text("Listing detail view...")
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
                 }
             }
         }

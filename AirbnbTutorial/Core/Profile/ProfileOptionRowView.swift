dfdb258e7f91b454e9eb088d13e0f3dc33857d67
Alyona <alyona.createbox@gmail.com>
2024-10-10 18:57:30 +0300

    Added UI ProfileView

---
+++ AirbnbTutorial/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbTutorial
+//
+//  Created by Alyona Voronina on 10.10.2024.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Settings")
+}

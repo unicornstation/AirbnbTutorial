ef8dbb50954cda08a5b30e6a39a8b35172cfb9cb
Alyona <alyona.createbox@gmail.com>
2024-11-22 17:58:45 +0300

    Added DeveloperPreview:  types of accommodation

--- AirbnbTutorial/Extantions/DeveloperPreview.swift
+++ AirbnbTutorial/Extantions/DeveloperPreview.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+class DeveloperPreview {
+    var listings: [Listing] = [
+        .init(
+            id: NSUUID().uuidString,
+            ownerUid: NSUUID().uuidString,
+            ownerName: "John Smith",
+            ownerImageUrl: "male-profile-photo",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 567,
+            latitude: 25.7850,
+            longitude: -80.1936, 
+            imageURLs: ["pool_h1", "room_h1", "beach_h1", "restaurant_h1"],
+            address: "124 Main St",
+            city: "Miami",
+            state: "Florida",
+            title: "Miami Villa",
+            rating: 4.86,
+            features: [.selfCheckIn, .superHost],
+            amenities: [.wifi, .alarmSystem, .balcony, .laundry, .tv],
+            type: .villa
+            ),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              ownerName: "Mari Loo",
+              ownerImageUrl: "male-profile-photo",
+              numberOfBedrooms: 3,
+              numberOfBathrooms: 2,
+              numberOfGuests: 6,
+              numberOfBeds: 6,
+              pricePerNight: 600,
+              latitude: 28.9834,
+              longitude: -85.2647, 
+              imageURLs: ["house_h2", "bedroom_h2"],
+              address: "125 Main St",
+              city: "Miami",
+              state: "Florida",
+              title: "Lovely House",
+              rating: 4.93,
+              features: [.selfCheckIn, .superHost],
+              amenities: [.alarmSystem, .balcony, .pool],
+              type: .house
+             )
+    ]
+}
